e7c952e5dfbe4a40384d5bfe06adde108a697969
Luke Truitt <luke@truittluke.com>
2021-03-24 22:50:24 -0600

    wklymtg

--- weekly_meeting.py
+++ weekly_meeting.py
@@ -5,3 +5,13 @@ def retrieve_weekly_events():
     # Go into the events table and look for all the ones whose date field is within the week
     # Return an array of dictionaries where each is an event with all of its parameters
     return []
+
+def get_initial_votes():
+    # pull up all deals that are at initial vote stage and pass them to the front, or deals that have recently been voted on
+    # return a list of dictionaries of these deals
+    return []
+
+def get_final_votes():
+    # pull up all deals that are at final vote stage and pass them to the front, or deals that have recently been voted on
+    # return a list of dictionaries of these deals
+    return []
