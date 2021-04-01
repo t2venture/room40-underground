f29ae7e542816e7444e92e89ab04a37237128cc3
Luke Truitt <luke@truittluke.com>
2021-03-31 21:37:28 -0600

    asdf

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+import os
 from flask import Flask
-app = Flask(__name__)
 
+
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+print(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
@@ -12,4 +16,6 @@ def hello_name(name):
     return "Hello {}!".format(name)
 
 if __name__ == '__main__':
+
     app.run()
+    
