b3a707cd899f32435a9e3d5ff22f8f90185c70b9
angikarghosal <angikar.ghosal@duke.edu>
2021-08-10 21:22:12 +0530

    This works, propertyportfolio not added yet

---
+++ app/main/model/property_portfolio.py
@@ -0,0 +1,11 @@
+from .. import db, flask_bcrypt
+import datetime
+import jwt
+from app.main.model.blacklist import BlacklistToken
+from ..config import key
+
+class PropertyPortfolio(db.Model):
+	"""Property Portfolio model for storing which property goes to which portfolio"""
+	__tablename__="property_portfolio"
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	
