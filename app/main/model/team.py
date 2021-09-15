f8c5a1265c9bf145f0bf1c678ab25a3b0fc6764b
angikarghosal <angikar.ghosal@duke.edu>
2021-09-16 03:41:41 +0530

    Rows added to team'

--- app/main/model/team.py
+++ app/main/model/team.py
@@ -10,3 +10,11 @@ class Team(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(255), unique=False, nullable=False)
+    color = db.Column(db.String(7), unique=False, nullable=False, default='000000')
+    #black is the default color
+    created_by=db.Column(db.Integer, db.ForeignKey('user.id'))
+    modified_by=db.Column(db.Integer, db.ForeignKey('user.id'))
+    created_time=db.Column(db.DateTime, unique=False, nullable=False)
+    modified_time=db.Column(db.DateTime, unique=False, nullable=False)
+    is_deleted=db.Column(db.Boolean, unique=False, nullable=False)
+    is_active=db.Column(db.Boolean, unique=False, nullable=False)
