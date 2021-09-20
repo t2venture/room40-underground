9ce45080b147bdf3c7e7f7ebb6db67a738133159
angikarghosal <angikar.ghosal@duke.edu>
2021-09-20 22:30:40 +0530

    Parametrize janitor

--- app/main/util/janitor.py
+++ app/main/util/janitor.py
@@ -3,6 +3,8 @@ import datetime
 
 from app.main import db
 
+time_param=2592000
+
 from app.main.model.document import Document
 from app.main.model.property import Property
 from app.main.model.user import User
@@ -27,31 +29,31 @@ def clean_database():
 	#259200 is the number of seconds in 30 days
 	for doc in all_documents:
 		delta=time_now-doc.modified_time
-		if delta.total_seconds()>2592000:
+		if delta.total_seconds()>time_param:
 			Document.query(id=doc.id).delete()
 			db.session.commit()
 
 	for prop in all_propertys:
 		delta=time_now-prop.modified_time
-		if delta.total_seconds()>259200:
+		if delta.total_seconds()>time_param:
 			Property.query(id=prop.id).delete()
 			db.session.commit()
 
 	for usr in all_users:
 		delta=time_now-usr.modified_time
-		if delta.total_seconds()>259200:
+		if delta.total_seconds()>time_param:
 			User.query(id=usr.id).delete()
 			db.session.commit()
 
 	for tm in all_teams:
 		delta=time_now=tm.modified_time
-		if delta.total_seconds()>259200:
+		if delta.total_seconds()>time_param:
 			Team.query(id=tm.id).delete()
 			db.session.commit()
 	
 	for utm in all_user_teams:
 		delta=time_now=utm.modified_time
-		if delta.total_seconds()>259200:
+		if delta.total_seconds()>time_param:
 			UserTeam.query(id=tm.id).delete()
 			db.session.commit()
 	
