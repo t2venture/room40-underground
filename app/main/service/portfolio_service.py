c8a17e0df9937ca94ac834c830a759460fbe4f96
angikarghosal <angikar.ghosal@duke.edu>
2021-09-24 04:06:06 +0530

    undo accidential delete

--- app/main/service/portfolio_service.py
+++ app/main/service/portfolio_service.py
@@ -6,15 +6,26 @@ from app.main.model.portfolio import Portfolio
 from app.main.model.property_portfolio import PropertyPortfolio
 from app.main.model.team_portfolio import TeamPortfolio
 from app.main.service.property_portfolio_service import get_portfolios_from_property
-from app.main.service.team_portfolio_service import get_portfolios_from_team, get_teams_from_portfolio
-
+from app.main.service.team_portfolio_service import get_portfolios_from_team, get_teams_from_portfolio, save_new_team_portfolio
+from app.main.service.team_service import get_personal_team_id
 def save_new_portfolio(data):
     try:
         new_portfolio = Portfolio(
             title=data['title'],
             description=data['description'],
+            created_by=data['login_user_id'],
+            modified_by=data['login_user_id'],
+            created_time=data['action_time'],
+            modified_time=data['action_time'],
+            is_deleted=False,
+            is_active=True,
         )
         save_changes(new_portfolio)
+        new_data=dict()
+        new_data["portfolio_id"]=new_portfolio.id
+        new_data["team_id"]=get_personal_team_id(data['login_user_id'])
+        new_data["role"]="Owner"
+        save_new_team_portfolio(new_data)
         response_object = {
                 'status': 'success',
                 'message': 'Successfully registered.',
@@ -32,8 +43,15 @@ def save_new_portfolio(data):
 def update_portfolio(portfolio_id, data):
     try:
         portfolio=get_a_portfolio(portfolio_id)
+        if 'title' not in data.keys():
+            data['title']=portfolio.title
+        if 'description' not in data.keys():
+            data['description']=portfolio.description
+            #####
         portfolio.title=data['title'],
         portfolio.description=data['description'],
+        portfolio.modified_by=data['login_user_id'],
+        portfolio.modified_time=data['action_time'], 
         save_changes(portfolio)
         response_object = {
                     'status': 'success',
@@ -49,10 +67,15 @@ def update_portfolio(portfolio_id, data):
         return response_object, 401
 
 
-def delete_a_portfolio(portfolio_id):
+def delete_a_portfolio(portfolio_id, data):
     try:
-        Portfolio.query.filter_by(id=portfolio_id).delete()
+        del_pfs=Portfolio.query.filter_by(id=portfolio_id).all()
+        for pf in del_pfs:
+            pf.is_deleted=True
+            pf.modified_time=data['action_time']
+            pf.modified_by=data['modified_by']
         db.session.commit()
+        #need to change this
         PropertyPortfolio.query.filter_by(portfolio_id=portfolio_id).delete()
         db.session.commit()
         TeamPortfolio.query.filter_by(portfolio_id=portfolio_id).delete()
@@ -71,9 +94,9 @@ def delete_a_portfolio(portfolio_id):
         }
         return response_object, 401
 
-def get_all_portfolios(property_id="", team_id=""):
+def get_all_portfolios(property_id="", team_id="", is_deleted=False, is_active=True):
     # Get all portfolios
-    portfolios=Portfolio.query
+    portfolios=Portfolio.query.filter_by(is_deleted=is_deleted).filter_by(is_active=is_active)
     if property_id and property_id!="":
         portfolio_ids=[pt.portfolio_id for pt in get_portfolios_from_property(property_id)]
         portfolios=portfolios.filter(Portfolio.id.in_(portfolio_ids))
@@ -82,12 +105,14 @@ def get_all_portfolios(property_id="", team_id=""):
         portfolios=portfolios.filter(Portfolio.id.in_(portfolios_ids))
     return portfolios.all()
 
+def get_all_deleted_portfolios():
+    return Portfolio.query.filter_by(is_deleted=True).all()
+
 
 def get_a_portfolio(portfolio_id):
-    return Portfolio.query.filter_by(id=portfolio_id).first()
+    return Portfolio.query.filter_by(id=portfolio_id).filter_by(is_deleted=False).filter_by(is_active=True).first()
 
 
 def save_changes(data):
     db.session.add(data)
     db.session.commit()
- 
