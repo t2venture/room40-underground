d4046182619e62d4972bba81bfcb1cb58d487610
angikarghosal <angikar.ghosal@duke.edu>
2021-09-23 18:09:17 +0530

    All setup EXCEPT the permission logic

--- app/main/controller/portfolio_controller.py
+++ app/main/controller/portfolio_controller.py
@@ -1,9 +1,12 @@
 from flask import request
 from flask_restplus import Resource, reqparse
-
+from ..util.decorator import token_required, admin_token_required
 from ..util.dto import PortfolioDto
 from ..service.portfolio_service import save_new_portfolio, get_all_portfolios, get_a_portfolio, update_portfolio, delete_a_portfolio
-
+from ..service.team_portfolio_service import save_new_team_portfolio
+from ..service.team_service import get_all_teams, get_a_team, get_personal_team_id
+from ..service.auth_helper import Auth
+import datetime
 api = PortfolioDto.api
 _portfolio = PortfolioDto.portfolio
 
@@ -13,22 +16,39 @@ class PortfolioList(Resource):
     @api.doc('list_of_portfolios for a portfolio_model')
     @api.param('property_id', 'property id to search portfolios for')
     @api.param('team_id', 'team_id to search portfolios for')
+    @api.param('is_deleted', 'if the portfolio is deleted or not')
+    @api.param('is_active', 'if the portfolio is active or not')
     @api.marshal_list_with(_portfolio, envelope='data')
     def get(self):
         """List all portfolios"""
         parser = reqparse.RequestParser()
         parser.add_argument("property_id", type=int)
         parser.add_argument("team_id", type=int)
+        parser.add_argument("is_deleted", type=bool)
+        parser.add_argument("is_active", type=bool)
         args = parser.parse_args()
-        return get_all_portfolios(args['property_id'], args['team_id'])
+        return get_all_portfolios(args['property_id'], args['team_id'], args["is_deleted"], args["is_active"])
 
     @api.response(201, 'portfolio successfully created.')
     @api.doc('create a new portfolio')
     @api.expect(_portfolio, validate=True)
+    @token_required
     def post(self):
         """Creates a new portfolio """
         data = request.json
+        logined, status = Auth.get_logged_in_user(request)
+        token=logined.get('data')
+        if not token:
+            return logined, status
+        login_user={"login_user_id": token['user_id']}
+        action_time={"action_time": datetime.datetime.utcnow()}
+        data.update(login_user)
+        data.update(action_time)
+        personal_team=get_personal_team_id(login_user)
+        personal_team_id={"personal_team_id": personal_team["id"]}
+        data.update(personal_team_id)
         return save_new_portfolio(data=data)
+        #This also creates the entry in TeamPortfolio as to which team member is the owner of the portfolio.
 
 @api.route('/<portfolio_id>')
 @api.param('portfolio_id', 'The portfolio identifier')
@@ -50,10 +70,27 @@ class Portfolio(Resource):
     def put(self, portfolio_id):
         """Update a portfolio"""
         data = request.json
+        logined, status = Auth.get_logged_in_user(request)
+        token=logined.get('data')
+        if not token:
+            return logined, status
+        login_user={"login_user_id": token['user_id']}
+        action_time={"action_time": datetime.datetime.utcnow()}
+        data.update(login_user)
+        data.update(action_time)
         return update_portfolio(portfolio_id, data)
 
     @api.response(201, 'portfolio successfully deleted.')
     @api.doc('delete a portfolio')
-    def delete(self, portfolio_id):
+    def delete(self, portfolio_id, data):
         """Delete a portfolio """
+        logined, status = Auth.get_logged_in_user(request)
+        token=logined.get('data')
+        if not token:
+            return logined, status
+        login_user={"login_user_id": token['user_id']}
+        action_time={"action_time": datetime.datetime.utcnow()}
+        data=dict()
+        data.update(login_user)
+        data.update(action_time)
         return delete_a_portfolio(portfolio_id)
--- app/main/controller/team_portfolio_controller.py
+++ app/main/controller/team_portfolio_controller.py
@@ -1,8 +1,11 @@
 from flask import request
-from flask_restplus import Resource
+from flask_restplus import Resource,reqparse
+from ..util.decorator import token_required, admin_token_required
 
 from ..util.dto import TeamPortfolioDto
 from ..service.team_portfolio_service import save_new_team_portfolio, get_all_team_portfolios, get_a_team_portfolio, update_team_portfolio, delete_a_team_portfolio
+from ..service.auth_helper import Auth
+import datetime
 
 api = TeamPortfolioDto.api
 _team_portfolio = TeamPortfolioDto.team_portfolio
@@ -11,10 +14,16 @@ _team_portfolio = TeamPortfolioDto.team_portfolio
 @api.route('/')
 class TeamPortfolioList(Resource):
     @api.doc('list_of_team_portfolios for a team_portfolio')
+    @api.param('is_deleted', 'whether the property model is deleted or not')
+    @api.param('is_active', 'whether the property model is active or not')
     @api.marshal_list_with(_team_portfolio, envelope='data')
     def get(self):
         """List all team_portfolios"""
-        return get_all_team_portfolios()
+        parser = reqparse.RequestParser()
+        parser.add_argument("is_deleted", type=bool)
+        parser.add_argument("is_active", type=bool)
+        args=parser.parse_args()
+        return get_all_team_portfolios(args["is_deleted"], args["is_active"])
 
     @api.response(201, 'team_portfolio successfully created.')
     @api.doc('create a new team_portfolio')
@@ -22,6 +31,14 @@ class TeamPortfolioList(Resource):
     def post(self):
         """Creates a new team_portfolio """
         data = request.json
+        logined, status = Auth.get_logged_in_user(request)
+        token=logined.get('data')
+        if not token:
+            return logined, status
+        login_user={"login_user_id": token['user_id']}
+        action_time={"action_time": datetime.datetime.utcnow()}
+        data.update(login_user)
+        data.update(action_time)
         return save_new_team_portfolio(data=data)
 
 @api.route('/<team_portfolio_id>')
@@ -44,10 +61,27 @@ class TeamPortfolio(Resource):
     def put(self, team_portfolio_id):
         """Update a team_portfolio """
         data = request.json
+        logined, status = Auth.get_logged_in_user(request)
+        token=logined.get('data')
+        if not token:
+            return logined, status
+        login_user={"login_user_id": token['user_id']}
+        action_time={"action_time": datetime.datetime.utcnow()}
+        data.update(login_user)
+        data.update(action_time)
         return update_team_portfolio(team_portfolio_id, data)
 
     @api.response(201, 'team_portfolio successfully deleted.')
     @api.doc('delete a team_portfolio')
-    def delete(self, team_portfolio_id):
+    def delete(self, team_portfolio_id, data):
         """Delete a team"""
+        logined, status = Auth.get_logged_in_user(request)
+        token=logined.get('data')
+        if not token:
+            return logined, status
+        data=dict()
+        login_user={"login_user_id": token['user_id']}
+        action_time={"action_time": datetime.datetime.utcnow()}
+        data.update(login_user)
+        data.update(action_time)	
         return delete_a_team_portfolio(team_portfolio_id)
