83b2ea46abf48e455f1e8617ddc7a015c2083a08
angikarghosal <angikar.ghosal@duke.edu>
2021-10-14 02:11:20 +0530

    Email setup done, generated token, but fraud check needs to be done

--- app/main/util/email.py
+++ app/main/util/email.py
@@ -2,25 +2,23 @@ import smtplib
 from email.message import EmailMessage
 import random
 import string
-
+EMAIL_ADDRESS = 'tryrenta@gmail.com'
+EMAIL_PASSWORD = 'vgnpapuiolnsgtoi'
 def set_password():
 	x = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
 	return x
 
 def send_email(email, password):
-	#TO DO NEED TO SETUP EMAIL FUNCTION
-	EMAIL_ADDRESS = 'your_email_address'
-	EMAIL_PASSWORD = 'your_app_password'
 
 	msg = EmailMessage()
-	msg['Subject'] = 'This is my first Python email'
+	msg['Subject'] = 'Lasso: Password Change Request'
 	msg['From'] = EMAIL_ADDRESS 
 	msg['To'] = email
-	msg.set_content('Hello your prior password was $this. Your new password is $this. Please change it')
+	content='''Hello, we have received a request from your email account to change your password.
+	Your new password is '''+password+'''. Please login using your new password.'''
+	msg.set_content()
 
 
 	with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
 	    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD) 
 	    smtp.send_message(msg)
-
-
