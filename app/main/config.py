a4ef1f8de021bc9fb4269640634042b870848533
angikarghosal <angikar.ghosal@duke.edu>
2021-10-26 17:46:00 +0530

    Push

--- app/main/config.py
+++ app/main/config.py
@@ -6,7 +6,8 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious_secret_key')
+    SECRET_KEY = 'my_precious_secret_key'
+    #SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious_secret_key')
     DEBUG = False
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
