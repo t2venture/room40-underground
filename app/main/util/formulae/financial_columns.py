8f07e8d30903ee6b0024f56e475d6f531c0c3e54
angikarghosal <angikar.ghosal@duke.edu>
2021-12-15 04:44:02 +0530

    Push new financial columns

---
+++ app/main/util/formulae/financial_columns.py
@@ -0,0 +1,10 @@
+import random
+market_value=350000
+ann_mortgage_cost = market_value/20
+estimated_rent = ann_mortgage_cost / 7
+hoa_fee = random.randint(400,800)
+hoa_rent = True
+est_property_tax = 0.0315 * market_value
+est_insurance= 0.007452 * market_value
+cap_rate = (estimated_rent - (ann_mortgage_cost/12) - hoa_fee - est_insurance - est_property_tax)/market_value
+yield_rate: cap_rate * random.randint(95,105)/100
